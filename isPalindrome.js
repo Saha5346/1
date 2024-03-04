2afe8ac42c1ce80b84f90ade39e19a7b39a6bef1
User <Saha20072007@mail.ru>
2024-03-04 10:01:42 +0300

    l

---
+++ isPalindrome.js
@@ -0,0 +1,13 @@
+function isPalindrome(arr){
+
+ const len= arr.length;
+
+if (len % 2==0)return false;
+
+    for (let i = 0; i < len /2; i++) {
+  if (arr[i] !==arr[len-i-1])return false;
+}
+    return true;
+}
+
+console.log(isPalindrome([1 ,2 ,3 ,6 ,3 ,2 ,1]))
